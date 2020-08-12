4fec545b73b382e1f3188eeb8f116be069165a12
leety007 <leety007@sk.com>
2020-08-12 14:08:54 +0900

    first commit

---
+++ src/main/java/local/PolicyHandler.java
@@ -0,0 +1,41 @@
+package local;
+
+import local.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+
+    @Autowired
+    DeliveryRepository deliveryRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverOrdered_Ship(@Payload Ordered ordered){
+
+       // if(ordered.isMe()){
+       //     System.out.println("##### listener Ship : " + ordered.toJson());
+       // }
+
+        // 물류사와 전문교환
+        if(ordered.isMe()){
+            Delivery delivery = new Delivery();
+            delivery.setOrderId(ordered.getId());
+            delivery.setStatus("SHIPPED");
+
+            deliveryRepository.save(delivery);
+        }
+
+
+    }
+
+}
